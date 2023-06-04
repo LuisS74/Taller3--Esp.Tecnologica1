c5fa23a9834a38988b51bae673a4a23b32815928
LuisS74 <l.salazar05@ufromail.cl>
2023-06-03 23:21:22 -0400

    Cambio en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-        <p>(Se necesita actualizar la página para ver cambios, por alguna razón)</p>
-        <router-link :to="{ name:'userview', params: { userid: '647178286edc199bff73e81f' }}">Juan Santos</router-link>
-        &nbsp;
-        <router-link :to="{ name:'userview', params: { userid: '647178286edc199bff73e81e' }}">Alexis Sanchez</router-link>
-        &nbsp;
-        <router-link :to="{ name:'userview', params: { userid: '647178286edc199bff73e820' }}">Rayo McQueen</router-link>
-
-  </div>
-  <div>
     <RouterView></RouterView>
   </div>
 </template>
